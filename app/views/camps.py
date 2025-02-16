0a6d642dac3bc0bbec879b6593012ee8d7658a5d
Tiago2006-5 <150469453+Tiago2006-5@users.noreply.github.com>
2025-02-16 20:20:37 +0100

    début de l'inscription de l'enfant au camp

--- app/views/camps.py
+++ app/views/camps.py
@@ -1,6 +1,7 @@
 from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for)
 from werkzeug.security import check_password_hash, generate_password_hash
 from app.db.db import get_db, close_db
+from app.utils import *
 import os
 
 camps_bp = Blueprint('camp', __name__, url_prefix = '/camps')
@@ -11,5 +12,36 @@ def camps():
 
     cursor = db.execute('Select * From Camps')
     g.camps = cursor.fetchall()
+    number = []
+    for camp in g.camps:
+        nb = db.execute("Select Count(*) From Inscriptions Where Id_camp = ?", (camp['Id_camp'],)).fetchone()[0]
+        number.append([camp['Id_camp'], nb])
     close_db()
+    g.nb = {id_camp: nb for id_camp, nb in number}
     return render_template('camps/camp.html')
+
+@camps_bp.route('/participer/<int:Id_parent>/<int:Id_camp>', methods=('GET', 'POST'))
+@login_required
+def participer(Id_parent, Id_camp):
+    db = get_db()
+    enfants = db.execute('SELECT * FROM Enfants WHERE Id_parent = ?',(Id_parent,)).fetchall()
+    if not enfants:
+        return redirect(url_for('/user/profile'))
+    enfants_infos = []
+    for enfant in enfants:
+        enfant_info = db.execute('SELECT * FROM Personne WHERE Id_personne = ?',(enfant['Id_personne'],)).fetchone()
+        if enfant_info:
+            enfants_infos.append(enfant_info)
+    camp = db.execute('Select * From Camps Where Id_camp = ?',(Id_camp,),).fetchone()
+
+    nb_participants = {}
+    for row in db.execute("SELECT Id_camp, COUNT(*) FROM Inscriptions GROUP BY Id_camp"):
+        nb_participants[row['Id_camp']] = row[1]
+
+    close_db()
+    close_db()
+    return render_template('camps/participer.html', camp = camp, enfants = enfants, enfants_infos = enfants_infos, nb = nb_participants)
+
+
+
+    
